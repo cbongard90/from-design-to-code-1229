a8a44a5c21fe352e5f7888d6b3ca9625be0e17e5
Christian Bongard <christian.bongard90@gmail.com>
2023-09-09 10:52:41 +0400

    AJAXified the search

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -11,6 +11,11 @@ export default class extends Controller {
   }
 
   update() {
-    console.log("Send an AJAX request")
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data
+      })
   }
 }
